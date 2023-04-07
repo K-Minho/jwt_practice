61631c8a3c66401a611fd956abc5030057af6606
K-Minho <alsgh9979@naver.com>
2023-04-07 09:08:42 +0900

    jwt

--- src/main/java/shop/mtcoding/jwtstudy/controller/UserController.java
+++ src/main/java/shop/mtcoding/jwtstudy/controller/UserController.java
@@ -22,30 +22,29 @@ public class UserController {
     private final HttpSession session;
 
     @GetMapping("/user") // 인증 필요
-    public ResponseEntity<?> user(){
+    public ResponseEntity<?> user() {
         // 권한처리 이 사람이 이 게시글의 주인
         LoginUser loginUser = (LoginUser) session.getAttribute("loginUser");
-        if(loginUser.getId() == 1) {
+        if (loginUser.getId() == 1) {
             return ResponseEntity.ok().body("접근 성공");
-        }else{
+        } else {
             return new ResponseEntity<>("접근 실패", HttpStatus.FORBIDDEN);
         }
-
 
     }
 
     @GetMapping("/") // 인증 불필요
-    public ResponseEntity<?> main(){
+    public ResponseEntity<?> main() {
         return ResponseEntity.ok().body("접근 성공");
     }
 
     @PostMapping("/login")
-    public ResponseEntity<?> login(User user){
+    public ResponseEntity<?> login(User user) {
         Optional<User> userOP = userRepository.findByUsernameAndPassword(user.getUsername(), user.getPassword());
-        if(userOP.isPresent()){
+        if (userOP.isPresent()) {
             String jwt = JwtProvider.create(userOP.get());
             return ResponseEntity.ok().header(JwtProvider.HEADER, jwt).body("로그인 성공");
-        }else{
+        } else {
             return ResponseEntity.badRequest().build();
         }
     }
